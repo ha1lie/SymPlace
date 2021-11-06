02886bb9af33569df51544c9aa99210b70a61977
Hallie <70453087+ha1lie@users.noreply.github.com>
2021-11-06 12:22:35 -0400

    Initial Commit

---
+++ SymPlace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SymPlace
+//
+//  Created by Hallie on 11/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SymPlace/SymPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymPlaceApp.swift
+//  SymPlace
+//
+//  Created by Hallie on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SymPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
